5cbf2b9d86063ad8edf1180f280cbc2f62840cfc
Rui Dias <rui.dias@weezie.io>
2025-10-02 21:59:15 +0100

    fix: Adding dark mode styling on NavItem

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,10 +2,12 @@ import { ThemeToggleButton } from "app/components/ThemeToggleButton";
 import { BookOpen } from "lucide-react";
 import { Sparkles } from "lucide-react";
 import { NavItem } from "app/components/NavItem";
+import { useTranslation } from "react-i18next";
 
 export default function NavBar() {
+  const { t } = useTranslation();
   return (
-    <nav className="h-16 bg-white/95 z-50 fixed top-0 dark:bg-slate-900/80 backdrop-blur-md border-b border-slate-200 w-full dark:border-slate-700  ">
+    <nav className="h-16 bg-white/90 z-50 fixed backdrop-blur-md top-0 dark:bg-slate-900/90 border-slate-200 w-full   ">
       <div className="flex items-center justify-between w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8 h-full">
         <div className="flex items-center gap-14">
           <div className="flex items-center gap-3">
@@ -14,9 +16,9 @@ export default function NavBar() {
               StudyAI
             </h1>
           </div>
-          <div className="flex items-center gap-6">
-            <NavItem icon={BookOpen} label="Decks" />
-            <NavItem icon={Sparkles} label="AI Generator" />
+          <div className="flex items-center gap-10">
+            <NavItem icon={BookOpen} label={t("Decks")} />
+            <NavItem icon={Sparkles} label={t("AI Generator")} />
           </div>
         </div>
 
--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -11,11 +11,11 @@ export function NavItem({ icon: Icon, label, onClick }: NavItemProps) {
     <button
       onClick={onClick}
       className="flex items-center gap-4 p-2 rounded-md 
-                 hover:bg-gray-200 dark:hover:bg-slate-800 
+                 hover:bg-gray-300 dark:hover:bg-slate-800 
                  cursor-pointer transition-colors"
     >
-      <Icon className="w-4 h-4" />
-      <span className="text-sm">{label}</span>
+      <Icon className="w-4 h-4 dark:text-white" />
+      <span className="text-sm dark:text-white">{label}</span>
     </button>
   );
 }
